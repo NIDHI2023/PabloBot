294a92da67bcbc62ead2c5701b4ee80e9e5d99b2
Shankar04 <shankarnilesh04@gmail.com>
2024-05-29 11:18:41 -0400

    init attempts at chat and weather

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from discord import Intents, Client, Message
 import os
+import logging
+import nextcord
+
+from discord import Intents, Client, Message
 from dotenv import load_dotenv
 from response import get_response
-import logging
+from nextcord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-
+'''
 intents = Intents.default()
 intents.message_content = True
 client = Client(intents=intents)
@@ -45,9 +49,36 @@ async def on_message(message: Message) -> None:
     logging.info(f'Message from {username} in {channel}: "{content}"')
     
     await send_response(message, message.content)
+'''
+
+bot = commands.Bot(command_prefix='!', intents=nextcord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
     
+@bot.command(name='quote')
+async def quote(ctx):
+    response = get_response(ctx.message, 'quote')
+    await ctx.send(response)
+
+@bot.command(name='nba')
+async def nba(ctx):
+    response = get_response(ctx.message, 'nba')
+    await ctx.send(response)
+    
+@bot.command(name='chat')
+async def chat(ctx, *, message):
+    response = get_response(ctx.message, f'chat {message}')
+    await ctx.send(response)
+    
+@bot.command(name='weather')
+async def weather(ctx, *, location):
+    response = get_response(ctx.message, f'weather {location}')
+    await ctx.send(response)
+
 def main():
-    client.run(token=TOKEN)  
+    bot.run(TOKEN) 
     
 if __name__ == '__main__':
     main()
--- response.py
+++ response.py
@@ -1,7 +1,10 @@
-from discord import Intents, Client, Message
 import requests
-from nba_scraper import get_nba_score
 import json
+from discord import Message
+import nba_scraper
+import chat_request
+import weather
+
 
 def get_quote() -> str:
     response = requests.get('https://zenquotes.io/api/random')
@@ -13,8 +16,12 @@ def get_response(message: Message, user_input: str) -> str:
     if (user_input == 'quote'):
         return get_quote()
     elif (user_input == 'nba'):
-        return get_nba_score()
+        return nba_scraper.get_nba_score()
+    elif (user_input.startswith('chat')):
+        return chat_request.get_chat_response(user_input[5:])
+    elif (user_input == 'weather'):
+        return weather.get_weather(user_input[8:])
     elif (user_input == 'help'):
-        return 'Available commands: quote, nba, help'
+        return 'Available commands: quote, nba, chat, weather, help'
     else:
         return 'bozo'
